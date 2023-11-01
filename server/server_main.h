c2a383e7eb2f6d3e8dca8f86f28d6c95a0f116a2
Dan Farber <fayan2@telem.openu.ac.il>
2023-11-01 15:35:41 +0200

    [Server - Bug Fix]: Fixed exit values

--- server/server_main.h
+++ server/server_main.h
@@ -3,9 +3,9 @@
 
 enum server_exit_value {
     SERVER_EXIT_PASS        = 0,
-    SERVER_EXIT_FAIL        = 0,
-    SERVER_EXIT_FAIL_INIT   = 0,
-    SERVER_EXIT_FAIL_LOGIC  = 0,
+    SERVER_EXIT_FAIL        = 1,
+    SERVER_EXIT_FAIL_INIT   = 2,
+    SERVER_EXIT_FAIL_LOGIC  = 3,
 };
 
 #endif /* SERVER_MAIN_H */
